fdf2f0a9d4b102cb2c8fd4cd80ce4f9519f561d1
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-04 11:22:16 +0200

    refactor app-bar as separate widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_social/models/app_pages.dart';
-import 'package:flutter_social/providers/app_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_social/widgets/flutter_social_appbar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,88 +15,17 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class MenuItems {
-  static const people = 'People';
-  static const myProfile = 'My Profile';
-  static const signOut = 'Sign Out';
-
-  static List<String> get smallScreen => [people, myProfile, signOut];
-  static List<String> get bigScreen => [myProfile, signOut];
-}
-
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    bool isSmallScreen = false;
-    if (MediaQuery.of(context).size.width < 600) isSmallScreen = true;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Social'),
-        automaticallyImplyLeading: false,
-        actions: [
-          isSmallScreen
-              ? IconButton(onPressed: () {}, icon: const Icon(Icons.home))
-              : InkWell(
-                  child: _buildMenuButton(icon: Icons.home, label: 'Home'),
-                  onTap: () {},
-                ),
-          _buildDropdownMenu(isSmallScreen),
-        ],
+    return const Scaffold(
+      appBar: PreferredSize(
+        child: FlutterSocialAppBar(),
+        preferredSize: Size.fromHeight(56.0),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Center(child: Text('Home')),
       ),
     );
-  }
-
-  PopupMenuButton<String> _buildDropdownMenu(bool isSmallScreen) {
-    var popUpMenuButton = isSmallScreen
-        ? PopupMenuButton<String>(
-            icon: const Icon(Icons.more_vert),
-            onSelected: _performAction,
-            itemBuilder: (_) => _buildMenuItems(MenuItems.smallScreen),
-          )
-        : PopupMenuButton<String>(
-            child:
-                _buildMenuButton(icon: Icons.account_circle, label: 'Account'),
-            onSelected: _performAction,
-            itemBuilder: (_) => _buildMenuItems(MenuItems.bigScreen),
-          );
-
-    return popUpMenuButton;
-  }
-
-  Center _buildMenuButton({required IconData icon, required String label}) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Row(
-          children: [
-            Icon(icon),
-            const SizedBox(width: 5.0),
-            Text(label),
-          ],
-        ),
-      ),
-    );
-  }
-
-  List<PopupMenuEntry<String>> _buildMenuItems(List<String> menuItems) {
-    return menuItems.map((item) {
-      return PopupMenuItem<String>(
-        value: item,
-        child: Text(item),
-      );
-    }).toList();
-  }
-
-  void _performAction(valueSelected) {
-    switch (valueSelected) {
-      case MenuItems.signOut:
-        Provider.of<AppProvider>(context, listen: false).logOut();
-        break;
-      default:
-    }
   }
 }
